2ad4ec210a15a72a597740a94429e8d18978ec6a
Reggie Mc <reggie.mcdonald@fbitn.com>
2020-01-30 15:38:49 -0600

    Repair Foundation Flex Grid
    Repair Foundation Flex Grid

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,11 +5,25 @@ class Home extends Component {
 
   render() {
     return (
-      <div className="content">
-        <h2>Home Directory</h2>
+      <div className="content-home">
+        {/* <h2>Home Directory</h2>
         <p>Culpa flank short ribs laboris ipsum anim labore ham hock nulla ball tip sirloin chislic.</p>
         <hr />
-        <h3>Featured Spicy jalapeno bacon</h3>
+        <h3>Featured Spicy jalapeno bacon</h3> */}
+
+        <div class="row medium-unstack">
+          <div class="columns">One</div>
+          <div class="columns">Two</div>
+          <div class="columns">Three</div>
+          <div class="columns">Four</div>
+          <div class="columns">Five</div>
+          <div class="columns">Six</div>
+        </div>
+
+        <div class="row">
+          <div class="columns shrink">Shrink!</div>
+          <div class="columns">Expand!</div>
+        </div>
 
       </div>
     );
